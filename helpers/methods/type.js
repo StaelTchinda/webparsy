177ccd991702b54d5f274a9945d752fca4984e46
StaelTchinda <45164908+StaelTchinda@users.noreply.github.com>
2024-01-09 15:53:27 +0100

    Allow env variables for values of type

--- helpers/methods/type.js
+++ helpers/methods/type.js
@@ -2,8 +2,12 @@ const schema = {
   method: 'type',
   process: async (flags, page, params, html, usingPuppeteer) => {
     const { selector, text, options } = params
-    if (!selector || !text) throw new Error('incorrect-type-options')
-    await page.type(selector, text.toString(), options || {})
+    if (!selector || !text) throw new Error('incorrect-type-options: ' + JSON.stringify(selector) + ", " + JSON.stringify(text))
+
+    // Check if the value is pointing to an environment variable
+    let textValue = text.env ? process.env[text.env] : text
+
+    await page.type(selector, textValue.toString(), options || {})
   }
 }
 
